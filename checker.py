b2558657dec38ea144f3dc493feebbdd8bfa61a6
PyKnight-vedant <hivedofficial@gmail.com>
2024-01-08 07:59:00 +0530

    Sai till 40250

--- checker.py
+++ checker.py
@@ -5,5 +5,6 @@ import numpy as np
 df = pd.read_csv(
     r"C:\Users\Vedant\Desktop\DataZenPrac\App2Build_Deliverable (4).csv", index_col=0)
 
+print(df)
 for i in df.columns:
     print(i, "-", df.loc[:, i].value_counts().sum())
